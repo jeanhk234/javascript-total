be3d1a3a2a2653d6fe022193eea0ba545d428094
ghostjoshux <joshuaquesada26@gmail.com>
2025-05-23 16:12:34 -0600

    cotidiano veterinario

---
+++ cotidiano_animales/misScripts.js
@@ -0,0 +1,13 @@
+function recomendar(mascotas){
+    let animales = document.getElementById("animales");
+
+    switch(mascotas) {
+        case 'perro':
+            if (animales = "lista"){
+                animales.textContent = "perro 1"
+            }
+
+
+
+    }
+}
