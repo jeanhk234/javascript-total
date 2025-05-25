c3a0393715c74733b19c6b55588e4608ca4c2480
Familia Bravo <pozojeancarlo@gmail.com>
2025-05-24 19:09:40 -0600

    trabajo cot

--- cotidiano_animales/misScripts.js
+++ cotidiano_animales/misScripts.js
@@ -1,13 +1,11 @@
-function recomendar(mascotas){
-    let animales = document.getElementById("animales");
+function listaAnimales() {
 
-    switch(mascotas) {
-        case 'perro':
-            if (animales = "lista"){
-                animales.textContent = "perro 1"
-            }
-
-
-
-    }
+    let perro = "simba " + "shin tzu " + "4"
+    let perro2 = " toyota " + "can " + "4"
+    let perro3 = " 4x4 " + "pitbull " + "4"
+    
+    document.write(perro)
+    document.write(perro2)
+    document.write(perro3)
 }
+
